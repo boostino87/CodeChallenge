c254b232e45ac8bc941d81cebd30837ad1099ffe
s.coluccia <ssalvatoreeco@gmail.com>
2018-03-15 17:47:45 +0100

    suca3

---
+++ CodeChallengeMeetingRooms/src/it/coluccia/codechallenge2018/boh/Assegnamento.java
@@ -0,0 +1,28 @@
+package it.coluccia.codechallenge2018.boh;
+
+public class Assegnamento {
+
+	private int indiceProvider;
+	private int indiceRegionProvider;
+	private int numPacchetti;
+	public int getIndiceProvider() {
+		return indiceProvider;
+	}
+	public void setIndiceProvider(int indiceProvider) {
+		this.indiceProvider = indiceProvider;
+	}
+	public int getIndiceRegionProvider() {
+		return indiceRegionProvider;
+	}
+	public void setIndiceRegionProvider(int indiceRegionProvider) {
+		this.indiceRegionProvider = indiceRegionProvider;
+	}
+	public int getNumPacchetti() {
+		return numPacchetti;
+	}
+	public void setNumPacchetti(int numPacchetti) {
+		this.numPacchetti = numPacchetti;
+	}
+	
+	
+}
---
+++ CodeChallengeMeetingRooms/src/it/coluccia/codechallenge2018/boh/OutputProject.java
@@ -0,0 +1,21 @@
+package it.coluccia.codechallenge2018.boh;
+
+import java.util.List;
+
+public class OutputProject {
+	
+	List<Assegnamento> assegnamenti;
+
+	public List<Assegnamento> getAssegnamenti() {
+		return assegnamenti;
+	}
+
+	public void setAssegnamenti(List<Assegnamento> assegnamenti) {
+		this.assegnamenti = assegnamenti;
+	}
+	
+	
+	
+	
+
+}
